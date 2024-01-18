5e19758d2afd1a1bbaf24c34fb656205345e4890
Eason Ong <eong@lcs.on.ca>
2024-01-18 14:07:18 -0500

    Initial Commit

---
+++ TradingCards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TradingCards
+//
+//  Created by Ong Eason on 18/1/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Ong Eason on 18/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
