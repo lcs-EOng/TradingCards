731ca2dd8043669ece25e4d753c144c8d09b3936
Eason Ong <eong@lcs.on.ca>
2024-01-18 15:03:32 -0500

    Added photo and stats for Thierry Henry

--- TradingCards/ContentView.swift
+++ TradingCards/ContentView.swift
@@ -11,37 +11,50 @@ struct TradinCard {
     let imageName: String
     let playerName: String
     let gamesPlayed: Double
-    let gamesStarted: Double
     let goalsScored: Double
     let assists: Double
     let yellowCards: Double
     let redCards: Double
-    let leagueRanking: String
+    let shots: Double
+    let shotsOnTarget: Double
+    let tackles: Double
 }
 struct DetailView: View {
     var body: some View{
         VStack(alignment: .leading){
+            Rectangle()
+                .foregroundColor(.white)
+                .frame(height: 75)
             Text("  Thierry Henry")
-                .font(.largeTitle)
-                .bold()
-                .frame(height: 150)
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundStyle(.black)
+                    .frame(height: 20)
             ZStack{
                 Rectangle()
                     .foregroundColor(.cyan)
+                    .frame(height: 500)
                     .ignoresSafeArea()
                 Rectangle()
                     .padding(25)
                     .foregroundColor(.white)
-                    .frame(height: 600)
-                Rectangle()
-                    .foregroundColor(.red)
                     .frame(height: 500)
+                Rectangle()
+                    .foregroundColor(.darkBlue)
+                    .frame(height: 420)
                     .padding(50)
-                
-                    
+                Rectangle()
+                    .padding(100)
+                    .foregroundColor(.white)
+                    .frame(height: 550)
             }
+            
+            
         }
-    }
+            Rectangle()
+                .foregroundColor(.white)
+                .frame(height: 200)
+        }
 }
 
 #Preview {
